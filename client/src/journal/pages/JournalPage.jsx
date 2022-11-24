cf766153deb6facc0b6b8d81ca281df656817faa
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-11-23 23:21:43 -0500

    Journal Layout and Journal Page

--- client/src/journal/pages/JournalPage.jsx
+++ client/src/journal/pages/JournalPage.jsx
@@ -1,9 +1,10 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../Layout/JournalLayout";
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>Pariatur culpa nulla minim nostrud ullamco id. Magna est consectetur occaecat irure et ullamco minim sit do. Veniam amet nostrud consectetur aliquip minim elit irure ut pariatur. Do exercitation ex reprehenderit ad ut consequat nulla enim esse ea.</Typography>
+    </JournalLayout>
   )
 };
