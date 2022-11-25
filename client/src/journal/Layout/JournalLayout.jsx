c80f069ba57b56097cc1e6fa76335097af30db43
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-11-25 00:10:42 -0500

    finished NavBar component

--- client/src/journal/Layout/JournalLayout.jsx
+++ client/src/journal/Layout/JournalLayout.jsx
@@ -5,7 +5,7 @@ const drawerWidth = 240;
 export const JournalLayout = ({children}) => {
   return (
     <Box sx={{display: "flex"}}>
-        <NavBar />
+        <NavBar drawerWidth={drawerWidth} />
         <Box 
         component="main"
         sx={{flexGrow: 1, p: 3}}
