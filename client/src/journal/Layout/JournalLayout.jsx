7cfd05293653f9abe767f9d4c0bfac78a5ad83f3
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-11-24 23:57:46 -0500

    NavBar component

--- client/src/journal/Layout/JournalLayout.jsx
+++ client/src/journal/Layout/JournalLayout.jsx
@@ -1,11 +1,11 @@
-import { Box } from "@mui/system"
+import { Box } from '@mui/system';
+import { NavBar } from '../components';
 
 const drawerWidth = 240;
 export const JournalLayout = ({children}) => {
   return (
     <Box sx={{display: "flex"}}>
-
-
+        <NavBar />
         <Box 
         component="main"
         sx={{flexGrow: 1, p: 3}}
