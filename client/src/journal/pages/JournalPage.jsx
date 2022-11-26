bb83564f616159f0d46da2f94992acbd57754a7c
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-11-25 21:21:53 -0500

    NoteView component created

--- client/src/journal/pages/JournalPage.jsx
+++ client/src/journal/pages/JournalPage.jsx
@@ -1,11 +1,12 @@
 import { JournalLayout } from "../Layout/JournalLayout";
-import { NothingSelectedView } from '../views';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography>Pariatur culpa nulla minim nostrud ullamco id. Magna est consectetur occaecat irure et ullamco minim sit do. Veniam amet nostrud consectetur aliquip minim elit irure ut pariatur. Do exercitation ex reprehenderit ad ut consequat nulla enim esse ea.</Typography> */}
-      <NothingSelectedView />
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   )
 };
