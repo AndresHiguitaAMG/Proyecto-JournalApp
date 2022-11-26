92695e4208c954e16d9058e5f7f2afaebcfd918d
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-11-25 19:03:03 -0500

    SideBar component implemented

--- client/src/journal/components/SideBar.jsx
+++ client/src/journal/components/SideBar.jsx
@@ -1,13 +1,41 @@
-import { Box, Drawer } from "@mui/material";
+import { TurnedInNot } from '@mui/icons-material';
+import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
 
-export const SideBar = ({ drawerWidth }) => {
+export const SideBar = ({ drawerWidth = 240 }) => {
   return (
     <Box
         component="nav"
         sx={{sm: {drawerWidth}, flexShrink: {sm: 0}}}
     >
-        <Drawer variant="permanent"> {/* Temporary */}
-        
+        <Drawer 
+          variant="permanent" //Temporary 
+          open
+          sx={{
+            display: {xs: "block"},
+            "& .MuiDrawer-paper": {boxSizing: "border-box", width: drawerWidth}
+          }}
+          >
+            <Toolbar>
+              <Typography variant="h6" nowRap component="div">Andres Higuita</Typography>
+            </Toolbar>
+            <Divider />
+            <List>
+              {
+                ["Enero", "Febrero", "Marzo", "Abril"].map(element => (
+                  <ListItem key={element} disablePadding>
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <TurnedInNot />
+                      </ListItemIcon>
+                      <Grid container>
+                        <ListItemText primary={element} />
+                        <ListItemText secondary={"Velit amet nisi non ullamco veniam."} />
+                      </Grid>
+                    </ListItemButton>
+                  </ListItem>
+                ))
+              }
+            </List>
         </Drawer>
     </Box>
   )
